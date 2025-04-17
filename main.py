ed9470e0c9b82f6a5a5098b83b83d5023abd2d36
Eleanor Wright <elliewright055@gmail.com>
2025-04-17 14:41:52 -0500

    Create convert_to_coords()

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 import requests
 import json
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 
 load_dotenv()
 
@@ -12,3 +14,13 @@ def get_weather_data(lat, lon):
 
     if response.status_code == 200:
         return response.json()
+
+def convert_to_coords(city_name):
+    api_key = os.getenv("API_KEY")
+    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={api_key}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        city_data = response.json()
+        lat, lon = city_data[0]["lat"], city_data[0]["lon"]
+        return (lat, lon)
